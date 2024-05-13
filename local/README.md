c944b5c96c0b8a96b3ea9ab6533b0706b6517c41
PPAPPO2 <0129harry@gmail.com>
2024-05-13 22:47:47 +0800

    update

---
+++ local/README.md
@@ -0,0 +1,4 @@
+# senior_project
+
+local 為主要檔案
+cat 是 app 名稱
---
+++ local/cat/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'homepage.html')
+def login(request):
+    return render(request,'login.html')
