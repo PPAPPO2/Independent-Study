f2721ac3d1fc0cf443ee24c8b12d21074947eec9
Eason Ji <a0984348986@gmail.com>
2024-05-20 18:23:28 +0800

    更新資料庫的資料連上前端顯示出來

--- local/cat/views.py
+++ local/cat/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """View function for home page of site."""
-
+    standings = TeamStanding23_24.objects.all()
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {'standings': standings})
 def login(request):
     return render(request,'login.html')
+
+from .models import TeamStanding23_24
