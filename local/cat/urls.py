f9c6e7c3d9e9e8d11f2c7dafe9b7e9f7c57e374c
XiaoNnnnn <nike0918nike@gmail.com>
2024-06-12 19:03:50 +0800

    新增JS+ShowMore頁面 代改

--- local/cat/urls.py
+++ local/cat/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path ('showMorePLG/', views.showMorePLG, name='showMorePLG'),
+    path ('showMore/', views.showMore, name='showMore'),
 ]
