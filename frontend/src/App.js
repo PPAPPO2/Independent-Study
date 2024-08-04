08afea07fc4d7d73dafc00e76d6bff887fd732a6
XiaoNnnnn <nike0918nike@gmail.com>
2024-08-04 19:55:22 +0800

    django結合react navbar可以連接但Standings不行

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Standings from "./components/Standings";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/cat" element={<Standings />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
