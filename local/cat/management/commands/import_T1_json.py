628e0b2b86811c1eab31d07749683ca85d18ae52
PPAPPO2 <0129harry@gmail.com>
2024-10-28 16:40:14 +0800

    推json

--- local/cat/management/commands/import_T1_json.py
+++ local/cat/management/commands/import_T1_json.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
 
-        output_dir = os.path.join('frontend/public/static/Standings', 'TeamData')
+        output_dir = os.path.join('frontend/public/static/Standings', 'PlayerData')
         # 檢查資料夾是否存在，如果不存在則創建
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -29,8 +29,8 @@ class Command(BaseCommand):
             #(T1_Season_Teams_Playoff_Performance_23_24,'T1_Season_Teams_Playoff_Performance_23_24.json'),
             #(T1_Season_Teams_Final_Performance_22_23,'T1_Season_Teams_Final_Performance_22_23.json'),
             #(T1_Season_Teams_Final_Performance_23_24,'T1_Season_Teams_Final_Performance_23_24.json'),
-            (TPBL_Season_Teams_Performance_24_25,'T1_Season_Teams_performance_24_25.json'),
-            #(TPBL_Season_Players_Performance_24_25,'T1_Players_performance_24_25.json'),
+            #(TPBL_Season_Teams_Performance_24_25,'T1_Season_Teams_performance_24_25.json'),
+            (TPBL_Season_Players_Performance_24_25,'T1_Players_performance_24_25.json'),
             #(T1_Season_Players_Final_Performance_22_23,'T1_Season_Players_Final_Performance_22_23.json'),
             #(T1_Season_Players_Final_Performance_23_24,'T1_Season_Players_Final_Performance_23_24.json'),
             #(T1_Season_Playoff_Players_Performance_22_23,'T1_Season_Playoff_Players_Performance_22_23.json'),
